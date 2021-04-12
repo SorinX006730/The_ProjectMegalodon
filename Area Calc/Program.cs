44769c61e8d53f37ef039b41c9ca6c220cf422ec
S✗R <51286027+SorinX006730@users.noreply.github.com>
2021-04-12 20:54:22 +0300

    Add files via upload

---
+++ Area Calc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Area_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert the height : ");
+            float Height = float.Parse(Console.ReadLine());
+
+            Console.Write("Insert the width : ");
+            float width = float.Parse(Console.ReadLine());
+
+            float areaCalc = Height * width;
+
+            Console.WriteLine($"This terrain has {areaCalc} square feets");
+        }
+    }
+}
