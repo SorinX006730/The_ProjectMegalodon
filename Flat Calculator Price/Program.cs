c8b67b6c34b15b6d2bc0ad3ee295ed13b444451e
S✗R <51286027+SorinX006730@users.noreply.github.com>
2021-04-12 21:05:32 +0300

    Add files via upload

---
+++ Flat Calculator Price/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Flat_Calculation_Price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert the height : ");
+            float Height = float.Parse(Console.ReadLine());
+
+            Console.Write("Insert the width : ");
+            float width = float.Parse(Console.ReadLine());
+
+            Console.Write("What is the cost of one square feet ? : ");
+            float squareFeetPrice = float.Parse(Console.ReadLine());
+
+            float areaCalc = Height * width;
+            float Price = areaCalc * squareFeetPrice;
+
+            Console.WriteLine($"This terrain has {areaCalc} square feets");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.Write($"This flat costs {Price} dollars");
+        }
+    }
+}
